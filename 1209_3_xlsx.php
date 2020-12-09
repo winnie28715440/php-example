6d84380fd2aeb6b72ee7a918ffc146e66acc821b
Winnie LIU <winnie287154400@gmail.com>
2020-12-09 13:33:09 +0800

    1209+

---
+++ 1209_3_xlsx.php
@@ -0,0 +1,15 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
+$spreadsheet = new Spreadsheet();
+$sheet = $spreadsheet->getActiveSheet();
+$sheet->setCellValue('A1', 'Hello World !');
+
+$writer = new Xlsx($spreadsheet);
+$writer->save('hello_world.xlsx');
+
+//終端機 php \1209_3_xlsx.php
